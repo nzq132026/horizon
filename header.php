c77dfe563ad310a550c9dd188f688d8ac3a5cb7b
nzq132026 <imailboxneil@gmail.com>
2017-11-17 15:19:15 +1100

    finish front-page

--- header.php
+++ header.php
@@ -10,34 +10,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-
     <header id="masthead" class="site-header" role="banner">
         <!-- Navigation -->
-        <nav class="navbar navbar-default">
-            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class=icon-bar></span>
-                <span class=icon-bar></span>
-                <span class=icon-bar></span>
-            </button>
-            <div class="navbar-header">
-                <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/header_logo.png" alt="logo image"></a>
-            </div>
-            <div class="navbar-collapse collapse" id="navbarResponsive">
-                                <?php wp_nav_menu(array(
-                                    'container' => false,
-                                    'theme_location' => 'header',
-                                    'menu_class' => 'nav navbar-nav dropdown navbar-right',
-                                    'walker' => new \vista\theme\MW_Walker_Nav_Menu()
-                                )
-                        );
-                    ?>
-            </div>
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a href="#"><img class="navbar-brand" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/header_logo.png" alt="logo image"></a>
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="row brand">     
+                    <div class="col-lg-4 col-md-4 col-sm-4 pull-left">
+                        <p><strong>VISTA SECURITY SOLUTIONS</strong></p>
+                    </div>
+                    <div class="col-lg-3 col-lg-offset-4 col-md-3 col-md-offset-2 col-sm-3 text-right">
+                        <p><strong>0414 266 788</strong></p>
+                    </div>
+                </div>
+                  <ul class="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">CCTV Packages</a></li>          
+                    <li><a href="#">Alarm Packages</a></li>
+                    <li><a href="#">Ducted Vaccum Systems</a></li>
+                    <li><a href="#">Security Door Intercom</a></li>
+                    <li><a href="#">Monitoring</a></li>
+                    <li><a href="#">Contact us</a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
         </nav>
+        <!---->
         <!-- Navigation Ends -->
     </header><!-- #masthead -->
-
-    <div class="site-content-contain">
-        <div id="content" class="site-content container-fluid">
 
